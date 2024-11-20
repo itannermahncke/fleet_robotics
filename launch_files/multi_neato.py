8ce89d23198aea649a508743267c0a951fd1cec4
cleira0501 <ariel920501@gmail.com>
2024-11-20 10:40:02 -0500

    updated launch file

--- launch_files/multi_neato.py
+++ launch_files/multi_neato.py
@@ -1,109 +1,45 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
-from launch_ros.actions import Node, PushRosNamespace
-from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 from ament_index_python.packages import get_package_share_directory
 
-# Helper function to create nodes for each robot
-def create_neato_nodes(robot_index, robot_name, udp_video_port, udp_sensor_port, host):
-    interfaces_launch_file_dir = os.path.join(get_package_share_directory('neato2_gazebo'), 'launch')
-
-    return GroupAction(actions=[
-        PushRosNamespace(namespace=robot_name),  # Use namespace for robot isolation
-        Node(
-            package='neato_node2',
-            executable='neato_node',
-            name='neato_driver',
-            parameters=[
-                {"use_udp": True},
-                {"udp_port": udp_sensor_port},
-                {"robot_name": robot_name},
-                {"host": host}
-            ],
-            output='screen'
-        ),
-        Node(
-            package='fix_scan',
-            executable='fix_scan',
-            name='fix_scan',
-            parameters=[{"robot_name": robot_name}]
-        ),
-        Node(
-            package='neato_node2',
-            executable='setup_udp_stream',
-            name='udp_stream_setup',
-            parameters=[
-                {"receive_port": udp_video_port},
-                {"width": 1024},
-                {"height": 768},
-                {"fps": 30},
-                {"host": host}
-            ],
-            output='screen'
-        ),
-        Node(
-            package='gscam',
-            executable='gscam_node',
-            parameters=[
-                {'preroll': True},
-                {'camera_name': 'camera'},
-                {'use_gst_timestamps': False},
-                {'frame_id': 'camera'},
-                {'gscam_config': f'udpsrc port={udp_video_port} ! application/x-rtp, payload=96 ! '
-                                 f'rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert'}
-            ]
-        )
-    ])
-
-# Main launch description
 def generate_launch_description():
-    # Declare number of robots
-    declare_robot_count = DeclareLaunchArgument(
-        'robot_count', default_value='2', description='Number of robots to launch'
+    # Declare the number of Neatos and a comma-separated list of IP addresses
+    num_neatos = DeclareLaunchArgument('num_neatos', default_value='2')
+    ip_addresses = DeclareLaunchArgument('ip_addresses', default_value='192.168.16.50,192.168.16.51')
+
+    # Directory of the bringup_multi.py file
+    bringup_multi_launch_dir = os.path.join(
+        get_package_share_directory('neato_2_gazebo'),'launch'
     )
 
-    robot_count = LaunchConfiguration('robot_count')
+    def create_robot_launch(context, *args, **kwargs):
+        num_robots = int(LaunchConfiguration('num_neatos').perform(context))
+        ips = LaunchConfiguration('ip_addresses').perform(context).split(',')
 
-    # Declare arguments common for all robots
-    robots = []
-    for i in range(10):  # Support up to 10 robots for this example
-        robots.append(
-            DeclareLaunchArgument(
-                f'robot{i}_name', default_value=f'robot{i}', description=f'Namespace for robot {i}'
+        actions = []
+        for i in range(num_robots):
+            robot_name = f"robot_{i}"
+            host = ips[i]
+            actions.append(
+                GroupAction([
+                    IncludeLaunchDescription(
+                        PythonLaunchDescriptionSource([bringup_multi_launch_dir, '/bringup_multi.py']),
+                        launch_arguments={
+                            'robot_name': TextSubstitution(text=robot_name),
+                            'host': TextSubstitution(text=host),
+                            'udp_video_port': TextSubstitution(text=str(5002 + i)),
+                            'udp_sensor_port': TextSubstitution(text=str(7777 + i)),
+                        }.items()
+                    )
+                ])
             )
-        )
-        robots.append(
-            DeclareLaunchArgument(
-                f'robot{i}_host', default_value=f'192.168.16.{50 + i}', description=f'Host IP for robot {i}'
-            )
-        )
-        robots.append(
-            DeclareLaunchArgument(
-                f'robot{i}_udp_video_port', default_value=str(5000 + i), description=f'Video port for robot {i}'
-            )
-        )
-        robots.append(
-            DeclareLaunchArgument(
-                f'robot{i}_udp_sensor_port', default_value=str(7000 + i), description=f'Sensor port for robot {i}'
-            )
-        )
+        return actions
 
-    # Dynamically add robot nodes based on robot_count
-    robot_nodes = []
-    for i in range(10):  # Up to 10 robots
-        robot_nodes.append(
-            GroupAction(
-                actions=create_neato_nodes(
-                    robot_index=i,
-                    robot_name=LaunchConfiguration(f'robot{i}_name'),
-                    udp_video_port=LaunchConfiguration(f'robot{i}_udp_video_port'),
-                    udp_sensor_port=LaunchConfiguration(f'robot{i}_udp_sensor_port'),
-                    host=LaunchConfiguration(f'robot{i}_host'),
-                ),
-                condition=IfCondition(f'{robot_count} > {i}'),  # Only include robots up to `robot_count`
-            )
-        )
-
-    return LaunchDescription([declare_robot_count] + robots + robot_nodes)
+    return LaunchDescription([
+        num_neatos,
+        ip_addresses,
+        OpaqueFunction(function=create_robot_launch)
+    ])
