ed2fcc69c70e886944d0b6549d648a5f113438c6
cleira0501 <ariel920501@gmail.com>
2024-12-05 12:00:33 -0500

    added barebone structure of pathplanning

---
+++ fleet_robotics/path_planning/functions.py
@@ -0,0 +1,33 @@
+import random
+
+def calculate_path(start, goal, current_map):
+"""
+path planning
+"""
+pass
+
+def check_conflict(my_position, other_positions):
+"""
+"""
+pass
+
+def generate_priority():
+"""
+generate random integer and publish their number to other neatos
+"""
+random_int = random.randint(1, 100)
+
+pass
+
+
+def move_or_not():
+"""
+intake other neato's random intergers and compare them to figure out a priority cue
+return true or false indicating if they can move or not
+
+"""
+pass
+
+
+
+
