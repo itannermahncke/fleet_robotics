bb26d49d4108966868c3b2d5e7acad1ea02f96d3
cleira0501 <ariel920501@gmail.com>
2024-12-07 21:09:48 -0500

    added psudocode for path planning

--- fleet_robotics/path_planning/functions.py
+++ fleet_robotics/path_planning/functions.py
@@ -1,32 +1,59 @@
 import random
+import math
 
-def calculate_path(start, goal, current_map):
-"""
-path planning
-"""
-pass
+class PathPlanningNode():
+    def __init__(self):
+        self.current_pos = []
 
-def check_conflict(my_position, other_positions):
-"""
-"""
-pass
+    
+    def path_planning(current_pos, goal_pos, lidar, step_size, threshold_to_goal, threshold_to_obstacle):
 
-def generate_priority():
-"""
-generate random integer and publish their number to other neatos
-"""
-random_int = random.randint(1, 100)
+        while distance(current_pos, goal_pos)>threshold_to_goal:
+            
+            vector_to_goal = normalize(goal_pos - current_pos)
+            next_position = current_pos + step_size * vector_to_goal
 
-pass
+            
+            if is_obstacle(next_position, obstacles):
+                #calculate all surrounding grid distance to goal and select the one that has the least distance
+                left_detour = calculate_detour(current_pos, direction="left", step_size)
+                right_detour = calculate_detour(current_pos, direction="right", step_size)
+                best_position = choose_best_option(left_detour, right_detour, goal_pos)
+            else:
+                best_position = next_position
+
+            current_pos = best_position
+            redraw_path(current_pos, goal_pos)
 
 
-def move_or_not():
-"""
-intake other neato's random intergers and compare them to figure out a priority cue
-return true or false indicating if they can move or not
 
-"""
-pass
+
+
+def check_conflict(my_position, *other_positions, threshold):
+    """
+    check if there are immediate conflicts, if yes, return True
+    """
+    for position in other_positions:
+        if math.dist(my_position, position)< threshold:
+            return True
+
+def generate_numbers():
+    """
+    generate random integer and publish their number to other neatos
+    """
+    random_int = random.randint(1, 100)
+    
+    return random_int
+
+def move_or_not(my_num, *numbers):
+    """
+    intake themselves and other neato's random intergers and compare them to figure out a priority cue
+    return true or false indicating if they can move or not
+    """
+    for number in numbers:
+        if my_num > number:
+            return True
+    
 
 
 
