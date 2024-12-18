4aeffc3f782c1d7d3a058fc9153af023b2189908
cmawn <cmawn@olin.edu>
2024-12-18 14:29:17 -0500

    added update

--- fleet_robotics/network_startup.py
+++ fleet_robotics/network_startup.py
@@ -22,29 +22,6 @@ class NetworkStartupNode(Node):
         """
         super().__init__("network_startup")
 
-        # robots go in order and send a message, then receive confirmation that
-        # it was heard.
-        # Robot 1: HELLO
-        # Robot 2: HELLO
-        # Robot 3: HELLO
-        # Robot 1: HEARD 2
-        # Robot 2: HEARD 2
-        # Robot 3: HEARD 2
-        # if anything is wrong here, the user should take the Neatos down
-        # then the neatos must agree on the current time
-        # Robot 1: START TIMERS
-        # Robot 1: [starts timer]
-        # Robot 2: [starts timer]
-        # Robot 3: [starts timer]
-        # Robot 1: MY TIME AFTER 1 CALLBACK
-        # Robot 2: MY TIME AFTER 1 CALLBACK
-        # Robot 3: MY TIME AFTER 1 CALLBACK
-
-        # then the neatos must take note of everyone else's offset relative to them
-        # Robot 1: [robot2_offset, robot3_offset]
-        # Robot 2: [robot1_offset, robot3_offset]
-        # Robot 3: [robot1_offset, robot2_offset]
-
         # Getting the number of robots
         self.declare_parameter("num_robots", rclpy.Parameter.Type.INTEGER)
         self.num_robots = (
