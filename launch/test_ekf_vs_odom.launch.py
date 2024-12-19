9dd26b878626ee597325b7b18f9fdb610d18a800
Ivy Mahncke <imahncke@olin.edu>
2024-12-19 12:23:10 -0500

    initial pose

--- launch/test_ekf_vs_odom.launch.py
+++ launch/test_ekf_vs_odom.launch.py
@@ -1,5 +1,6 @@
 from launch_ros.actions import Node
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
 
 import os
 
@@ -9,12 +10,16 @@ def generate_launch_description():
     name_str = "robot1"
     robot_name = {"robot_name": name_str}
 
+    fleet_info = os.path.join(
+        get_package_share_directory("fleet_robotics"), "fleet_info.yaml"
+    )
+
     return LaunchDescription(
         [
             Node(
                 package="fleet_robotics",
                 executable="odom_adapter",
-                parameters=[robot_name],
+                parameters=[robot_name, fleet_info],
             ),
             Node(
                 package="fleet_robotics",
